28ef663d6b86ca059a7233eff1adf845e7dc0544
Callen Schultz <86019388+hollapena@users.noreply.github.com>
2021-12-20 23:41:16 -0600

    updated welcome screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
 import Welcome from './components/Welcome';
 import Signup from './components/Signup';
 import Login from "./components/Login";
@@ -6,17 +7,17 @@ import Login from "./components/Login";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
-      <div>
-        <Welcome />
-        <Signup />
-        <Login />
-      </div>
-      <footer className="App-footer">
 
-      </footer>
+      <header className="App-header"></header>
+
+      <Routes>
+        <Route id="welcome-screen" path="/" element={<Welcome />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+
+      <footer className="App-footer"></footer>
+
     </div>
   );
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,20 +1,27 @@
-import React, { Component } from 'react'
+import React from "react";
 
-export class Welcome extends Component {
-
-  constructor(props){
-    super(props)
-    this.state={
-      
-    }
-  }
-  render() {
-    return (
-      <div>
-        
+function Welcome() {
+  return (
+    <div id="welcome-screen">
+      {/* <img
+        id="suitcase"
+        src="https://i.pinimg.com/originals/4c/b0/3e/4cb03e76f409d854eeda76a7b840f23a.gif"
+        alt="swinging suitcase"
+      ></img> this option requires align items start and display flex row in the css file*/}
+      <div id="welcomeitems">
+        <h1>Welcome</h1>
+        <hr id="welcomeline"></hr>
+        <br></br>
+        <button>Log In</button>
+        <br></br>
+        <br></br>
+        <button>Sign Up</button>
+        <br></br>
+        <br></br>
+        <button>Practice List</button>
       </div>
-    )
-  }
+    </div>
+  );
 }
 
-export default Welcome
+export default Welcome;
